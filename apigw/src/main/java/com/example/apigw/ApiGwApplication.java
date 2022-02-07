da9042bff56b982f09c611c71c99c36b26f0863b
Toutsu <hegin4@gmail.com>
2022-02-07 23:48:15 +0300

    Add gateway API

---
+++ apigw/src/main/java/com/example/apigw/ApiGwApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwApplication.class, args);
+    }
+}
