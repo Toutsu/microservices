5d980e22c9f8f65da119852026945c0a7c9cc63d
Toutsu <hegin4@gmail.com>
2022-02-10 14:07:05 +0300

    refactor microservices to kube

--- clients/src/main/java/com/example/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/example/clients/notification/NotificationClient.java
@@ -3,7 +3,7 @@ package com.example.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(name = "notification", url = "${clients.notification.url}")
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
     void sendNotification(NotificationRequest notificationRequest);
--- notification/src/main/java/com/example/notification/NotificationApplication.java
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -3,6 +3,8 @@ package com.example.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,19 +13,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
         }
 )
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-    //Testing RabbitMQ
-//    @Bean
-//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
-//                                        NotificationConfig notificationConfig) {
-//        return args -> {
-//            producer.publish("foo",
-//                    notificationConfig.getInternalExchanges(),
-//                    notificationConfig.getInternalNotificationRoutingKey());
-//        };
-//    }
 }
